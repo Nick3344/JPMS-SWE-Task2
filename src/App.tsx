d32b93b6991a46f338d05125c93658fe2e6e1c56
Nikhil Gowda <114958232+Nick3344@users.noreply.github.com>
2024-04-04 11:13:11 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'; 
 import DataStreamer, {ServerRespond} from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
